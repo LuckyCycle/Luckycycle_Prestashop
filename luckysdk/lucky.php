8eb931aa183a6d6624f54f93f0a2e373d9204f01
Guillaume Sartenaer <tiguss@gmail.com>
2015-11-04 10:52:11 +0100

    Update lucky.php

--- luckysdk/lucky.php
+++ luckysdk/lucky.php
@@ -10,9 +10,13 @@ class LuckyCycleApi extends PestJSON
 	public function __construct($base_url=DEV_URL)
 	{
 		parent::__construct($base_url);
-		//$this->mode = 
-	}
+		//$this->mode =
+		$this->content = array();
+		$this->cart = array();
 
+	}
+	public $content;
+	public $cart;
 	/**
 	 * @var string api key
 	 */
@@ -33,28 +37,39 @@ class LuckyCycleApi extends PestJSON
 	}
 
 	public function setOperationId($operation_id) {
-		$this->operation_id = $operation_id;	
+		$this->operation_id = $operation_id;
 	}
 
 	public function show() {
 		$path = '/api/v1/operations/' . $this->operation_id . '/show';
 		$data = array();
 		$headers = array( 'X-LuckyApiKey'  =>  $this->api_key );
-		
+
 		return $this->get($path, $data, $headers);
+	}
+	public function get_content(){
+		return $this->content;
+	}
+	public function set_content($value){
+		$this->content = $value;
+	}
+
+	public function add_cart_item($item) {
+		array_push($this->cart, $item);
+		$this->content['cart'] = $this->cart;
 	}
 
 	public function poke($data) {
 		$path = '/api/v1/operations/' . $this->operation_id . '/poke';
 		$headers = array( 'X-LuckyApiKey'  =>  $this->api_key );
-		
+
 		return $this->post($path, $data, $headers);
 	}
 
 	public function draw($data) {
 		$path = '/api/v1/operations/' . $this->operation_id . '/draw';
 		$headers = array( 'X-LuckyApiKey'  =>  $this->api_key );
-		
+
 		return $this->post($path, $data, $headers);
 	}
 
